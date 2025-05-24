bf99d012fb17b8244bab16627830ce6d4ba6c5a2
calumhrmurray <calumhrmurray@gmail.com>
2025-05-24 14:06:51 +0200

    for some reason cf_bgs has broken, no idea why

---
+++ scripts/measure_correlation_functions.py
@@ -0,0 +1,15 @@
+import astropy.io.fits as fits
+import astropy.coordinates as coord
+import astropy.units as u
+import numpy as np
+import treecorr
+import matplotlib.pyplot as plt
+import healpy as hp
+
+# specify shape catalogue, position catalogue, and random catalogues
+
+# give settings
+
+# measure the correlation function
+
+# save the correlation function
